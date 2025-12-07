373b0e8f09d38d405627f6464501b693ab28d3df
David Bauch <jamesbauch@gmail.com>
2025-12-07 08:25:33 -0700

    tweak: playwright.config.ts, default workers and 2 retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  retries: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   timeout: 60_000,
   expect: {
