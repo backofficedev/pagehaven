5b543c8e0ef651d6d8eb1885944a6d15344bba98
David Bauch <jamesbauch@gmail.com>
2025-12-15 11:02:49 -0700

    fix(static): remove domain from static since records are handled by site creation api

--- apps/static/alchemy.run.ts
+++ apps/static/alchemy.run.ts
@@ -37,13 +37,11 @@ const envBindings = {
   BETTER_AUTH_URL,
   BETTER_AUTH_SECRET,
 } as const;
-const domains = isDevelopmentEnvironment(stage) ? undefined : [STATIC_DOMAIN];
 const zone = await createZone(STATIC_DOMAIN);
 const routes = isDevelopmentEnvironment(stage)
   ? undefined
   : [{ pattern: `*${STATIC_DOMAIN}/*`, zoneId: zone.id }];
 
-console.log(`Domains -> ${domains}`);
 console.log(`Routes -> ${routes}`);
 for (const [key, value] of Object.entries(envBindings)) {
   console.log(`${key} -> ${value}`);
@@ -54,7 +52,6 @@ const bindings = { DB: db, STORAGE: storage, CACHE: cache, ...envBindings };
 export const staticWorker = await createWorker<typeof bindings>({
   port: PORT,
   entrypoint: path.join(import.meta.dirname, "src/index.ts"),
-  domains,
   routes,
   bindings,
 });
